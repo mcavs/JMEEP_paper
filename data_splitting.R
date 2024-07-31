957a0cf4d57ea8def621d54246192c926fa4dad7
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2024-07-31 17:35:01 +0300

    Update data_splitting.R

--- data_splitting.R
+++ data_splitting.R
@@ -1,8 +1,3 @@
-#set.seed(123)
-#data_split <- initial_split(data = data, prop = 0.80) 
-#train_data <- data_split |> training()
-#test_data  <- data_split |> testing()
-
 train_data <- data |> 
   filter(code_presentation == "2013J") |>
   select(-code_presentation)
